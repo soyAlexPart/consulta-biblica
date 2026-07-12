a9e6d3ebb8c9662877d3166021ce5537787957df
Alex Part <lexirtozpad@gmail.com>
2026-07-11 21:52:04 -0600

    Implementa salto de liena en multiversiculos, alineacion mas a la derecha en los versiculos en pagina web

--- data.js
+++ data.js
@@ -12,9 +12,9 @@ const bibliotecaBiblica = [
     {
         nombre: "¿Por que no pedir prestado?",
         versiculos: [
-            { ref: "Salmos 37:21", texto: "El impío toma prestado, y no paga; Mas el justo tiene misericordia, y da." },
-            { ref: "Romanos 13:8", texto: "No debáis a nadie nada, sino el amaros unos a otros; porque el que ama al prójimo, ha cumplido la ley." },
-            { ref: "Proverbios 19:14", texto: "La casa y las riquezas son herencia de los padres; Mas de Jehová la mujer prudente." }
+            { ref: "Proverbios 22:26-27", texto: "26. No seas de aquellos que se comprometen, Ni de los que salen por fiadores de deudas.\n\n27. Si no tuvieres para pagar, ¿Por qué han de quitar tu cama de debajo de ti?" },
+            { ref: "Proverbios 11:15", texto: "Con ansiedad será afligido el que sale por fiador de un extraño; Mas el que aborreciere las fianzas vivirá seguro." },
+            { ref: "Proverbios 22:7", texto: "El rico se enseñorea de los pobres, Y el que toma prestado es siervo del que presta." }
         ]
     }
 
