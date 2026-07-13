bc652c187e076a1935c99b1ff366a86148c61950
Alex Part <lexirtozpad@gmail.com>
2026-07-12 20:40:52 -0600

    Implementa tema la falsamente llamada ciencia

--- data.js
+++ data.js
@@ -2,9 +2,9 @@ const bibliotecaBiblica = [
     {
         nombre: "¿Por que no prestar?",
         versiculos: [
-            { ref: "Salmos 37:21", texto: "El impío toma prestado, y no paga; Mas el justo tiene misericordia, y da." },
-            { ref: "Romanos 13:8", texto: "No debáis a nadie nada, sino el amaros unos a otros; porque el que ama al prójimo, ha cumplido la ley." },
-            { ref: "Proverbios 19:14", texto: "La casa y las riquezas son herencia de los padres; Mas de Jehová la mujer prudente." }
+            { ref: "Salmos 37:21", texto: "21. El impío toma prestado, y no paga; Mas el justo tiene misericordia, y da." },
+            { ref: "Romanos 13:8", texto: "8. No debáis a nadie nada, sino el amaros unos a otros; porque el que ama al prójimo, ha cumplido la ley." },
+            { ref: "Proverbios 19:14", texto: "14. La casa y las riquezas son herencia de los padres; Mas de Jehová la mujer prudente." }
         ]
     },
 
@@ -12,16 +12,16 @@ const bibliotecaBiblica = [
         nombre: "¿Por que no pedir prestado?",
         versiculos: [
             { ref: "Proverbios 22:26-27", texto: "26. No seas de aquellos que se comprometen, Ni de los que salen por fiadores de deudas.\n\n27. Si no tuvieres para pagar, ¿Por qué han de quitar tu cama de debajo de ti?" },
-            { ref: "Proverbios 11:15", texto: "Con ansiedad será afligido el que sale por fiador de un extraño; Mas el que aborreciere las fianzas vivirá seguro." },
-            { ref: "Proverbios 22:7", texto: "El rico se enseñorea de los pobres, Y el que toma prestado es siervo del que presta." }
+            { ref: "Proverbios 11:15", texto: "15. Con ansiedad será afligido el que sale por fiador de un extraño; Mas el que aborreciere las fianzas vivirá seguro." },
+            { ref: "Proverbios 22:7", texto: "7. El rico se enseñorea de los pobres, Y el que toma prestado es siervo del que presta." }
         ]
     },
 
     {
     nombre: "No prestar a usura",
     versiculos: [
-        { ref: "Ezequiel 18:1-13", texto: "1. Vino a mí palabra de Jehová, diciendo:\n\n2. ¿Qué pensáis vosotros, los que usáis este refrán sobre la tierra de Israel, que dice: Los padres comieron las uvas agrias, y los dientes de los hijos tienen la dentera?\n\n3. Vivo yo, dice Jehová el Señor, que nunca más tendréis por qué usar este refrán en Israel.\n\n4. He aquí que todas las almas son mías; como el alma del padre, así el alma del hijo es mía; el alma que pecare, esa morirá.\n\n5. Y el hombre que fuere justo, e hiciere según el derecho y la justicia;\n\n6. que no comiere sobre los montes, ni alzare sus ojos a los ídolos de la casa de Israel, ni violare la mujer de su prójimo, ni se llegare a la mujer menstruosa,\n\n7. ni oprimiere a ninguno; que al deudor devolviere su prenda, que no cometiere robo, y que diere de su pan al hambriento y cubriere al desnudo con vestido,\n\n8. que no prestare a interés ni tomare usura; que de la maldad retrajere su mano, e hiciere juicio verdadero entre hombre y hombre,\n\n9. en mis ordenanzas caminare, y guardare mis decretos para hacer rectamente, éste es justo; éste vivirá, dice Jehová el Señor.\n\n10. Mas si engendrare hijo ladrón, derramador de sangre, o que haga alguna cosa de estas,\n\n11. y que no haga las otras, sino que comiere sobre los montes, o violare la mujer de su prójimo,\n\n12. al pobre y menesteroso oprimiere, cometiere robos, no devolviere la prenda, o alzare sus ojos a los ídolos e hiciere abominación,\n\n13prestare a interés y tomare usura; ¿vivirá éste? No vivirá. Todas estas abominaciones hizo; de cierto morirá, su sangre será sobre él." },
-        { ref: "Proverbios 28:8", texto: "El que aumenta sus riquezas con usura y crecido interés, Para aquel que se compadece de los pobres las aumenta."}
+        { ref: "Ezequiel 18:1-13", texto: "1. Vino a mí palabra de Jehová, diciendo:\n\n2. ¿Qué pensáis vosotros, los que usáis este refrán sobre la tierra de Israel, que dice: Los padres comieron las uvas agrias, y los dientes de los hijos tienen la dentera?\n\n3. Vivo yo, dice Jehová el Señor, que nunca más tendréis por qué usar este refrán en Israel.\n\n4. He aquí que todas las almas son mías; como el alma del padre, así el alma del hijo es mía; el alma que pecare, esa morirá.\n\n5. Y el hombre que fuere justo, e hiciere según el derecho y la justicia;\n\n6. que no comiere sobre los montes, ni alzare sus ojos a los ídolos de la casa de Israel, ni violare la mujer de su prójimo, ni se llegare a la mujer menstruosa,\n\n7. ni oprimiere a ninguno; que al deudor devolviere su prenda, que no cometiere robo, y que diere de su pan al hambriento y cubriere al desnudo con vestido,\n\n8. que no prestare a interés ni tomare usura; que de la maldad retrajere su mano, e hiciere juicio verdadero entre hombre y hombre,\n\n9. en mis ordenanzas caminare, y guardare mis decretos para hacer rectamente, éste es justo; éste vivirá, dice Jehová el Señor.\n\n10. Mas si engendrare hijo ladrón, derramador de sangre, o que haga alguna cosa de estas,\n\n11. y que no haga las otras, sino que comiere sobre los montes, o violare la mujer de su prójimo,\n\n12. al pobre y menesteroso oprimiere, cometiere robos, no devolviere la prenda, o alzare sus ojos a los ídolos e hiciere abominación,\n\n13. prestare a interés y tomare usura; ¿vivirá éste? No vivirá. Todas estas abominaciones hizo; de cierto morirá, su sangre será sobre él." },
+        { ref: "Proverbios 28:8", texto: "8. El que aumenta sus riquezas con usura y crecido interés, Para aquel que se compadece de los pobres las aumenta."}
     ]
 },
 
@@ -29,28 +29,63 @@ const bibliotecaBiblica = [
     nombre: "El discípulo amado",
     versiculos: [
         {
-    ref: "Juan 13:21-26",
-    texto: `21. Habiendo dicho Jesús esto, se conmovió en espíritu, y declaró y dijo:
-        <span class="letra-roja">De cierto, de cierto os digo, que uno de vosotros me va a entregar.</span>\n
-            22. Entonces los discípulos se miraban unos a otros, dudando de quién hablaba.\n
-            23. Y uno de sus discípulos, al cual Jesús amaba, estaba recostado al lado de Jesús.\n
-            24. A éste, pues, hizo señas Simón Pedro, para que preguntase quién era aquel de quien hablaba.\n
-            25. El entonces, recostado cerca del pecho de Jesús, le dijo: Señor, ¿quién es?\n
-            26. Respondió Jesús: <span class="letra-roja">A quien yo diere el pan mojado, aquél es.</span>\n Y mojando el pan, lo dio a Judas Iscariote hijo de Simón.`
-},
-        { ref: "Juan 21:20-24", texto: "jhdkufhd"},
-        { ref: "Juan 20:1-9", texto: "" }
+            ref: "Juan 13:21-26",
+            texto: `21. Habiendo dicho Jesús esto, se conmovió en espíritu, y declaró y dijo:
+            <span class="letra-roja">De cierto, de cierto os digo, que uno de vosotros me va a entregar.</span>\n
+                    22. Entonces los discípulos se miraban unos a otros, dudando de quién hablaba.\n
+                    23. Y uno de sus discípulos, al cual Jesús amaba, estaba recostado al lado de Jesús.\n
+                    24. A éste, pues, hizo señas Simón Pedro, para que preguntase quién era aquel de quien hablaba.\n
+                    25. El entonces, recostado cerca del pecho de Jesús, le dijo: Señor, ¿quién es?\n
+                    26. Respondió Jesús: <span class="letra-roja">A quien yo diere el pan mojado, aquél es.</span>\n Y mojando el pan, lo dio a Judas Iscariote hijo de Simón.`
+        },
+        { 
+            ref: "Juan 21:20-24",
+            texto: `20. Volviéndose Pedro, vio que les seguía el discípulo a quien amaba Jesús, el mismo que en la cena se había recostado al lado de él, y le había dicho: Señor, ¿quién es el que te ha de entregar?\n
+                    21. Cuando Pedro le vio, dijo a Jesús: Señor, ¿y qué de éste?\n
+                    22. Jesús le dijo: <span class="letra-roja">Si quiero que él quede hasta que yo venga, ¿qué a ti? Sígueme tú.</span>\n
+                    23. Este dicho se extendió entonces entre los hermanos, que aquel discípulo no moriría. Pero Jesús no le dijo que no moriría, sino: Si quiero que él quede hasta que yo venga, ¿qué a ti?\n
+                    24. Este es el discípulo que da testimonio de estas cosas, y escribió estas cosas; y sabemos que su testimonio es verdadero.`
+        },
+        {
+            ref: "Juan 20:1-9",
+            texto: `1. El primer día de la semana, María Magdalena fue de mañana, siendo aún oscuro, al sepulcro; y vio quitada la piedra del sepulcro.\n
+                    2. Entonces corrió, y fue a Simón Pedro y al otro discípulo, aquel al que amaba Jesús, y les dijo: Se han llevado del sepulcro al Señor, y no sabemos dónde le han puesto.\n
+                    3. Y salieron Pedro y el otro discípulo, y fueron al sepulcro.\n
+                    4. Corrían los dos juntos; pero el otro discípulo corrió más aprisa que Pedro, y llegó primero al sepulcro.\n
+                    5. Y bajándose a mirar, vio los lienzos puestos allí, pero no entró.\n
+                    6. Luego llegó Simón Pedro tras él, y entró en el sepulcro, y vio los lienzos puestos allí,\n
+                    7. y el sudario, que había estado sobre la cabeza de Jesús, no puesto con los lienzos, sino enrollado en un lugar aparte.\n
+                    8. Entonces entró también el otro discípulo, que había venido primero al sepulcro; y vio, y creyó.\n
+                    9. Porque aún no habían entendido la Escritura, que era necesario que él resucitase de los muertos.`
+        },
     ]
 },
 
 {
     nombre: "La falsamente llamada ciencia",
     versiculos: [
-        { ref: "1 Corintios 12:8", texto: "" },
-        { ref: "2 Crónicas 1:10-12", texto: "" },
-        { ref: "Eclesiastés 7:11-12", texto: "" },
-        { ref: "Eclesiastés 10:19", texto: "" },
-        { ref: "Proverbios 8:12", texto: "" }
+        { ref: "1 Corintios 12:8",
+            texto: `8. Porque a éste es dada por el Espíritu palabra de sabiduría; a otro, palabra de ciencia según el mismo Espíritu;`
+        },
+        { 
+            ref: "2 Cronicas 1:10-12",
+            texto: `10. Dame ahora sabiduría y ciencia, para presentarme delante de este pueblo; porque, quién podrá gobernar a este tu pueblo tan grande?\n
+                    11. Y dijo Dios a Salomón: por cuanto hubo esto en tu corazón, y no pediste riquezas, bienes o gloria, ni la vida de los que te quieren mal, ni pediste muchos días, sino que has pedido para ti sabiduría y ciencia para gobernar a mi pueblo, sobre el cual te he puesto por rey,\n
+                    12. sabiduría y ciencia te son dadas; y también te daré riquezas, bienes y gloria, como nunca tuvieron los reyes que han sido antes de ti, ni tendrán los que vengan después de ti.`
+        },
+        {
+            ref: "Eclesiastés 7:11-12",
+            texto: `11. Porque el sabio ve el día y la noche, y el necio ve el día y la noche, y no entiende nada.\n
+                    12. Y en la noche, cuando los hombres se juntan para comer, y beber, y se alegran con la música, no hay que olvidar que Dios es quien da la vida a todos los hombres.\n`
+        },
+        {
+            ref: "Eclesiastés 10:19",
+            texto: `19. El que tiene grandes riquezas, no puede disfrutar de ellas; ni el que tiene muchos hijos, puede vivir de ellas.\n`
+        },
+        {
+            ref: "Proverbios 8:12",
+            texto: `12. Yo soy sabiduría; yo soy entendimiento; yo soy consejo; yo soy poder.\n`
+        }
     ]
 },
 ];
