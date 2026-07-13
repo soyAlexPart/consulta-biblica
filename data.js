0f79aca7ce73d469296c4740e84c69d1ba9c2124
Alex Part <lexirtozpad@gmail.com>
2026-07-12 20:06:48 -0600

    Implementa tema el discipulo amado primer versiculo con las letras en rojo, solo en las palabras de Jesus

--- data.js
+++ data.js
@@ -21,15 +21,24 @@ const bibliotecaBiblica = [
     nombre: "No prestar a usura",
     versiculos: [
         { ref: "Ezequiel 18:1-13", texto: "1. Vino a mí palabra de Jehová, diciendo:\n\n2. ¿Qué pensáis vosotros, los que usáis este refrán sobre la tierra de Israel, que dice: Los padres comieron las uvas agrias, y los dientes de los hijos tienen la dentera?\n\n3. Vivo yo, dice Jehová el Señor, que nunca más tendréis por qué usar este refrán en Israel.\n\n4. He aquí que todas las almas son mías; como el alma del padre, así el alma del hijo es mía; el alma que pecare, esa morirá.\n\n5. Y el hombre que fuere justo, e hiciere según el derecho y la justicia;\n\n6. que no comiere sobre los montes, ni alzare sus ojos a los ídolos de la casa de Israel, ni violare la mujer de su prójimo, ni se llegare a la mujer menstruosa,\n\n7. ni oprimiere a ninguno; que al deudor devolviere su prenda, que no cometiere robo, y que diere de su pan al hambriento y cubriere al desnudo con vestido,\n\n8. que no prestare a interés ni tomare usura; que de la maldad retrajere su mano, e hiciere juicio verdadero entre hombre y hombre,\n\n9. en mis ordenanzas caminare, y guardare mis decretos para hacer rectamente, éste es justo; éste vivirá, dice Jehová el Señor.\n\n10. Mas si engendrare hijo ladrón, derramador de sangre, o que haga alguna cosa de estas,\n\n11. y que no haga las otras, sino que comiere sobre los montes, o violare la mujer de su prójimo,\n\n12. al pobre y menesteroso oprimiere, cometiere robos, no devolviere la prenda, o alzare sus ojos a los ídolos e hiciere abominación,\n\n13prestare a interés y tomare usura; ¿vivirá éste? No vivirá. Todas estas abominaciones hizo; de cierto morirá, su sangre será sobre él." },
-        { ref: "Proverbios 28:8", texto: "A", rojo: true }
+        { ref: "Proverbios 28:8", texto: "El que aumenta sus riquezas con usura y crecido interés, Para aquel que se compadece de los pobres las aumenta."}
     ]
 },
 
 {
     nombre: "El discípulo amado",
     versiculos: [
-        { ref: "Juan 13:21-26", texto: "" },
-        { ref: "Juan 21:20-24", texto: "" },
+        {
+    ref: "Juan 13:21-26",
+    texto: `21. Habiendo dicho Jesús esto, se conmovió en espíritu, y declaró y dijo:
+        <span class="letra-roja">De cierto, de cierto os digo, que uno de vosotros me va a entregar.</span>\n
+            22. Entonces los discípulos se miraban unos a otros, dudando de quién hablaba.\n
+            23. Y uno de sus discípulos, al cual Jesús amaba, estaba recostado al lado de Jesús.\n
+            24. A éste, pues, hizo señas Simón Pedro, para que preguntase quién era aquel de quien hablaba.\n
+            25. El entonces, recostado cerca del pecho de Jesús, le dijo: Señor, ¿quién es?\n
+            26. Respondió Jesús: <span class="letra-roja">A quien yo diere el pan mojado, aquél es.</span>\n Y mojando el pan, lo dio a Judas Iscariote hijo de Simón.`
+},
+        { ref: "Juan 21:20-24", texto: "jhdkufhd"},
         { ref: "Juan 20:1-9", texto: "" }
     ]
 },
@@ -44,7 +53,4 @@ const bibliotecaBiblica = [
         { ref: "Proverbios 8:12", texto: "" }
     ]
 },
-
-
-
 ];
